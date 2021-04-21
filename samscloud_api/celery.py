d74ca27736d4c2bf23c2b07d14b2f6b660fbeb99
Dmytro Kartamyshev <dmytro.kartamyshev@gmail.com>
2021-04-21 22:55:08 +0300

    Fix celery config

--- samscloud_api/celery.py
+++ samscloud_api/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samscloud_api.staging')
@@ -14,7 +15,7 @@ app = Celery('samscloud_api')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
